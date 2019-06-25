f32839cf9cf2f5bdd04e661724ad05ab9378b681
yxiaocai <44401411+yxiaocai@users.noreply.github.com>
2019-06-25 21:34:05 +0800

    Create iweread.js

---
+++ js/iweread.js
@@ -0,0 +1,29 @@
+var body = $response.body;
+const path1 = "/pay/memberCardSummary";
+const path2 = "/pay/balance";
+var path = $requests.path;
+
+function modify_card() {
+  let obj = JSON.parse(body);
+  obj["remainCoupon"] = 99999;
+  obj["expiredTime"] = 1594223999;
+  obj["expired"] = 0;
+  obj["remainTime"] = 1330440;
+  body = JSON.stringify(obj);
+}
+
+function modify_balance() {
+  let obj = JSON.parse(body);
+  obj["balance"] = 200;
+  obj["giftBalance"] = 100;
+  obj["peerBalance"] = 100;
+  body = JSON.stringify(obj);
+   }
+   
+if (path = path1){
+  modify_card();
+}
+if (path = path2){
+  modify_balance();
+}
+$done(body);
