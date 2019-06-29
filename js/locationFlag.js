5cecb2e7590335a606b41d07ce98e529b7518ba1
yxiaocai <44401411+yxiaocai@users.noreply.github.com>
2019-06-29 17:31:45 +0800

    Update locationFlag.js

--- js/locationFlag.js
+++ js/locationFlag.js
@@ -4,9 +4,9 @@ if ($response.statusCode !=200){
 var body = $response.body;
 var obj = JSON.parse(body);
 // var country = obj["country"]
-var title = obj["country"];
+var subtitle = obj["country"];
 var flagDict = {"Hong Kong": "🇭🇰", "Japan": "🇯🇵", "South Korea": "🇰🇷", "Taiwan": "🇹🇼", "United States": "🇺🇸", "Singapore": "🇸🇬", "Australia": "🇦🇺", "Germany": "🇩🇪", "United Kingdom": "🇬🇧", "France": "🇫🇷", "Thailand": "🇹🇭"};
-var subtitle = flagDict[title];
+var title = flagDict[subtitle];
 var ip = obj["query"];
 var description = "description";
 
